16ab9b56d06529ddfb1fa5cbd25cbbd42c8c0f23
Jaiiaj21 <jaijai211075@gmail.com>
2023-11-13 21:37:55 +0700

    add click animation for btn

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
 var buttonColours = ['red', 'blue', 'green', 'yellow'];
 gamePattern = [];
-$("#play-button").click(function() {
-    $("#play-button").addClass("clicked")
-    setTimeout(function () {
-        $("#play-button").removeClass("clicked");
-    }, 75);
-})
+for(let i = 0; i < buttonColours.length; i++){
+    $("#" + buttonColours[i]).click(function (){
+        playAnimation(buttonColours[i]);
+    });
+}
 
 
 function nextSequence() {
     var randomNumber = Math.floor(Math.random() * 4);
     var randomChosenColour = buttonColours[randomNumber];
     gamePattern.push(randomChosenColour);
+}
+
+function playSound() {
+
+}
+
+function playStartAnimation(){
+    $("#play-button").addClass("clicked");
+    setTimeout(function () {
+        $("#play-button").removeClass("clicked");
+    }, 75);
+}
+
+function playAnimation(color){
+    $("#" + color).addClass("clicked");
+    setTimeout(function () {
+        $("#" + color).removeClass("clicked");
+    }, 75);
 }
