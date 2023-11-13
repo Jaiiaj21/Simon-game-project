f136698d6cf4d4b8a9e4de31262deac37948e040
Jai <jaijai211075@gmail.com>
2023-11-14 01:53:41 +0700

    almost done

--- index.js
+++ index.js
@@ -1,32 +1,107 @@
-var buttonColours = ['red', 'blue', 'green', 'yellow'];
-gamePattern = [];
+const buttonColours = ['red', 'blue', 'green', 'yellow'];
+let gamePattern = [];
+let userClickedPattern = [];
+let level = 0;
+let started = false;
+
+// step 1 Create a new pattern.
+function nextSequence() {
+    let randomNumber = Math.floor(Math.random() * 4);
+    let randomChosenColour = buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour);
+    // from step 2 add sounds for new pattern button.
+    animatePattern(randomChosenColour);
+    playSound(randomChosenColour);
+    // increase level by 1 and change level-title up to now.
+    level++;
+    $(".level-title h2").text("level " + level);
+    // reset userClickesPattern for new level
+    userClickedPattern = [];
+
+}
+
+// step 2 Show the sequence to the user with animations and sounds.
+function playSound(sound) {
+    let audio = new Audio("./assets/sounds/" + sound + ".mp3");
+    audio.play();
+}
+function animatePattern(currentColour){
+    $("#" + currentColour).addClass("lightUp");
+    setTimeout(function () {
+        $("#" + currentColour).removeClass("lightUp");
+    }, 100);
+}
+
+// step 3 Check which button is pressed.
 for(let i = 0; i < buttonColours.length; i++){
     $("#" + buttonColours[i]).click(function (){
-        playAnimation(buttonColours[i]);
+        if(started){
+            let userChosenColour = buttonColours[i];
+            userClickedPattern.push(userChosenColour);
+            // console.log(userClickedPattern);
+            // from step 2,4 add sounds for button that user click.
+            animatePress(userChosenColour);
+            playSound(userChosenColour);
+            // from 6 check current level
+            checkAnswer(userClickedPattern.length - 1);
+        }
     });
 }
-
-
-function nextSequence() {
-    var randomNumber = Math.floor(Math.random() * 4);
-    var randomChosenColour = buttonColours[randomNumber];
-    gamePattern.push(randomChosenColour);
-}
-
-function playSound() {
-
-}
-
-function playStartAnimation(){
-    $("#play-button").addClass("clicked");
+// step 4 Add anitmations to user clicks.
+function animatePress(currentColour){
+    $("#" + currentColour).addClass("clicked");
     setTimeout(function () {
-        $("#play-button").removeClass("clicked");
-    }, 75);
+        $("#" + currentColour).removeClass("clicked");
+    }, 100);
 }
 
-function playAnimation(color){
-    $("#" + color).addClass("clicked");
-    setTimeout(function () {
-        $("#" + color).removeClass("clicked");
-    }, 75);
+
+// step 5 Start the game.
+$("#white").click(function() {
+    // play sound and animation when user click start game.
+    if(!started){
+        playSound("start");
+        animatePress("white");
+        setTimeout(function() {
+            $(".level-title h2").text("Level " + level);
+            nextSequence();
+        }, 1000);
+        // show first pattern.
+        started = !started;
+    }
+});
+
+// step 6 Check the user'answer against the game sequence.
+// if success go nextSquence
+// else game-over
+function checkAnswer(currentLevel) {
+    // user success this current level
+    if(gamePattern[currentLevel] === userClickedPattern[currentLevel]){
+        // success this level
+        if(currentLevel + 1 === level){
+            setTimeout(function() {
+                nextSequence();
+            }, 1000);
+        }
+    }else{
+        gameOver();
+    }
+}
+
+// step 7 Game over.
+function gameOver() {
+    $("body").addClass("game-over");
+    setTimeout(function() {
+        $("body").removeClass("game-over");
+    }, 150);
+    playSound("wrong");
+    $(".level-title").html("<h2>Game over, press <span>white</span> button to restart</h2>");
+    startOver();
+}
+
+// step 8 Restart the game.
+function startOver() {
+    level = 0;
+    gamePattern = [];
+    started = false;
 }
